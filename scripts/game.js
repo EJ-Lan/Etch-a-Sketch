349ec28233d617ff8d88f5dd00e2e1b75ef6bb52
Ervin Lanada <ervinjon_lanada@outlook.com>
2023-12-04 20:29:12 -0700

    Work so far

---
+++ scripts/game.js
@@ -0,0 +1,11 @@
+let gridNum = 16; // Number of square divs
+
+// Need to change this to be a grid
+let createGrid = (gridNum) => {
+    const gridContainer = document.querySelector('.grid-container');
+    for (let curGrid = 0; curGrid < gridNum; curGrid++) {
+        let gridSquare = document.createElement('div');
+        gridSquare.classList.add('grid-square');
+        gridContainer.appendChild(gridSquare);
+    }
+}
