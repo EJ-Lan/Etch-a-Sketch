66a6e2aad84ffc7dfc420680c5e53a22fcb50253
EJ-Lan <ervinjon_lanada@outlook.com>
2023-12-05 01:19:02 -0700

    createGrid function

--- scripts/game.js
+++ scripts/game.js
@@ -1,12 +1,19 @@
-
 let gridNum = 16; // Number of square divs
 
-// Need to change this to be a grid
 let createGrid = (gridNum) => {
     const gridContainer = document.querySelector('.grid-container');
-    for (let curGrid = 0; curGrid < gridNum; curGrid++) {
-        let gridSquare = document.createElement('div');
-        gridSquare.classList.add('grid-square');
-        gridContainer.appendChild(gridSquare);
+    
+    for (let rowIndex = 0; rowIndex < gridNum; rowIndex++) {
+        const gridRow = document.createElement('div');
+        gridRow.classList.add('grid-row');
+
+        for (let colIndex = 0; colIndex < gridNum; colIndex) {
+            const gridCol = document.createElement('div');
+            gridCol.classList.add('grid-col');
+            gridRow.appendChild(gridCol);
+        }
+        gridContainer.appendChild(gridRow);
     }
 }
+
+createGrid(gridNum);
